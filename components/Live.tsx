7cdc53043f907b349a915f7aeccb49ae35d141a8
Andrew Brought <andrewbrought@Andrews-MacBook-Pro.local>
2024-02-06 12:01:41 -0600

    almost completed the live reaction animation-need to fix the bug using the e key

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,23 +1,69 @@
-import { useMyPresence, useOther, useOthers } from "@/liveblocks.config"
+import { useBroadcastEvent, useEventListener, useMyPresence, useOther, useOthers } from "@/liveblocks.config"
 import LiveCusors from "./cursor/LiveCusors"
 import { useCallback, useEffect, useState } from "react";
 import CursorChat from "./cursor/CursorChat";
-import { CursorMode } from "@/types/type";
+import { CursorMode, CursorState, Reaction, ReactionEvent } from "@/types/type";
+import ReactionSelector from "./reaction/ReactionButton";
+import Cursor from "./cursor/Cursor";
+import FlyingReaction from "./reaction/FlyingReaction";
+import useInterval from "@/hooks/useInterval";
 
 const Live = () => {
     const others = useOthers();
     const [{ cursor }, updateMyPresence] = useMyPresence() as any;
-    const [cursorState, setCursorState] = useState({
+    const [cursorState, setCursorState] = useState<CursorState>({
         mode: CursorMode.Hidden,
     });
+
+    const [reaction, setReaction] = useState<Reaction[]>([]);
+
+    // Display flying reactions to group
+    const broadcast = useBroadcastEvent();
+
+    // Clear the state memory of the reaction mode
+    useInterval(() => {
+        // we clear the reactions that are completly unvisible by 4 seconds old
+        setReaction((reaction) => reaction.filter((r) => r.timestamp > Date.now() - 4000))
+    }, 1000)
+
+    // This is the state that manages the reaction mode including rate of reaction
+    useInterval(() => {
+        if (cursorState.mode === CursorMode.Reaction && cursorState.isPressed && cursor) {
+            setReaction((reactions) => reactions.concat([{
+                point: { x: cursor.x, y: cursor.y },
+                value: cursorState.reaction,
+                timestamp: Date.now(),
+            }]))
+
+            broadcast({
+                x: cursor?.x,
+                y: cursor?.y,
+                value: cursorState.reaction,
+            })
+        }
+
+    }, 20);
+
+    // This is the listener that allows the rest of the group to see the reaction
+    useEventListener((eventData) => {
+        const event = eventData.event as ReactionEvent;
+
+        setReaction((reactions) => reactions.concat([{
+            point: { x: event.x, y: event.y },
+            value: event.value,
+            timestamp: Date.now(),
+        }]))
+    })
 
     const handlePointerMove = useCallback((event: React.PointerEvent) => {
         event.preventDefault();
 
-        const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
-        const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+        if (cursor === null || cursorState.mode !== CursorMode.ReactionSelector) {
+            const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+            const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
-        updateMyPresence({ cursor: { x, y } });
+            updateMyPresence({ cursor: { x, y } });
+        }
 
     }, [])
 
@@ -28,6 +74,12 @@ const Live = () => {
 
     }, [])
 
+    const handlePointerUp = useCallback((event: React.PointerEvent) => {
+        // This manages the state change of triggering the reaction mode using 'e' key
+        // We add the 'isPressed' property in case we're dealing with the reaction state
+        setCursorState((state: CursorState) => cursorState.mode === CursorMode.Reaction ? { ...state, isPressed: true } : state);
+    }, [cursorState.mode, setCursorState])
+
     const handlePointerDown = useCallback((event: React.PointerEvent) => {
 
         const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
@@ -35,7 +87,10 @@ const Live = () => {
 
         updateMyPresence({ cursor: { x, y } });
 
-    }, [])
+        // This manages the state change of triggering the reaction mode using 'e' key
+        setCursorState((state: CursorState) => cursorState.mode === CursorMode.Reaction ? { ...state, isPressed: true } : state);
+
+    }, [cursorState.mode, setCursorState])
 
     // This manages the state change of triggering the chat mode using '/' key
     useEffect(() => {
@@ -49,6 +104,10 @@ const Live = () => {
             } else if (e.key === 'Escape') {
                 updateMyPresence({ message: '' });
                 setCursorState({ mode: CursorMode.Hidden })
+            } else if (e.key === 'e') {
+                setCursorState({
+                    mode: CursorMode.ReactionSelector,
+                })
             }
         };
 
@@ -66,23 +125,49 @@ const Live = () => {
         };
     }, [updateMyPresence]);
 
-    
+    // We created to this to monitor the state of the reaction mode exclusivley
+    const setReactions = useCallback((reaction: string) => {
+        setCursorState({ mode: CursorMode.Reaction, reaction, isPressed: false });
+    }, [])
+
 
     return (
         <div
             onPointerMove={handlePointerMove}
             onPointerLeave={handlePointerLeave}
             onPointerDown={handlePointerDown}
+            onPointerUp={handlePointerUp}
             className="h-[100vh] w-full flex justivfy-center items-center text-center"
         >
             <h1 className="text-2xl text-white">B2U-Figma</h1>
+
+            {reaction.map((r) => (
+                <FlyingReaction
+                    key={r.timestamp.toString()}
+                    x={r.point.x}
+                    y={r.point.y}
+                    timestamp={r.timestamp}
+                    value={r.value}
+                />
+            ))}
+
+
             {cursor &&
                 <CursorChat
                     cursor={cursor}
                     cursorState={cursorState}
                     setCursorState={setCursorState}
                     updateMyPresence={updateMyPresence}
-                />}
+                />
+            }
+
+            {/* Reaction Selector and Flying Reactions implementation */}
+            {cursorState.mode === CursorMode.ReactionSelector && (
+                <ReactionSelector
+                    setReaction={setReactions}
+                />
+            )}
+
             <LiveCusors others={others} />
         </div>
     )
