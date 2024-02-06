d25328faa785526578e35f9388a144fe8cd59a30
Andrew Brought <andrewbrought@Andrews-MacBook-Pro.local>
2024-02-06 10:50:05 -0600

    completed the cusror chat component

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,10 +1,15 @@
 import { useMyPresence, useOther, useOthers } from "@/liveblocks.config"
 import LiveCusors from "./cursor/LiveCusors"
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
+import CursorChat from "./cursor/CursorChat";
+import { CursorMode } from "@/types/type";
 
 const Live = () => {
     const others = useOthers();
     const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+    const [cursorState, setCursorState] = useState({
+        mode: CursorMode.Hidden,
+    });
 
     const handlePointerMove = useCallback((event: React.PointerEvent) => {
         event.preventDefault();
@@ -17,7 +22,7 @@ const Live = () => {
     }, [])
 
     const handlePointerLeave = useCallback((event: React.PointerEvent) => {
-        event.preventDefault();
+        setCursorState({ mode: CursorMode.Hidden });
 
         updateMyPresence({ cursor: null, message: null });
 
@@ -32,17 +37,55 @@ const Live = () => {
 
     }, [])
 
-  return (
-    <div
-        onPointerMove={handlePointerMove}
-        onPointerLeave={handlePointerLeave}
-        onPointerDown={handlePointerDown}
-        className="h-[100vh] w-full flex justivfy-center items-center text-center"
-    >
-        <h1 className="text-2xl text-white">B2U-Figma</h1>
-        <LiveCusors others={others} />
-    </div>
-  )
+    // This manages the state change of triggering the chat mode using '/' key
+    useEffect(() => {
+        const onKeyUp = (e: KeyboardEvent) => {
+            if (e.key === '/') {
+                setCursorState({
+                    mode: CursorMode.Chat,
+                    previousMessage: null,
+                    message: '',
+                })
+            } else if (e.key === 'Escape') {
+                updateMyPresence({ message: '' });
+                setCursorState({ mode: CursorMode.Hidden })
+            }
+        };
+
+        const onKeyDown = (e: KeyboardEvent) => {
+            if (e.key === '/') {
+                e.preventDefault();
+            }
+        };
+
+        window.addEventListener('keyup', onKeyUp);
+        window.addEventListener('keydown', onKeyDown);
+        return () => {
+            window.removeEventListener('keyup', onKeyUp);
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [updateMyPresence]);
+
+    
+
+    return (
+        <div
+            onPointerMove={handlePointerMove}
+            onPointerLeave={handlePointerLeave}
+            onPointerDown={handlePointerDown}
+            className="h-[100vh] w-full flex justivfy-center items-center text-center"
+        >
+            <h1 className="text-2xl text-white">B2U-Figma</h1>
+            {cursor &&
+                <CursorChat
+                    cursor={cursor}
+                    cursorState={cursorState}
+                    setCursorState={setCursorState}
+                    updateMyPresence={updateMyPresence}
+                />}
+            <LiveCusors others={others} />
+        </div>
+    )
 }
 
 export default Live
